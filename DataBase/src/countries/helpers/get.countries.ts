2f9025ba3cf9a1329c3632ce27bf2d9a84d97772
Tiago Sousa <ttiagopintosousa97@gmail.com>
2023-12-27 21:30:55 +0000

    DataBase - get coutries order fix

--- DataBase/src/countries/helpers/get.countries.ts
+++ DataBase/src/countries/helpers/get.countries.ts
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import * as fs from 'fs';
-import * as path from 'path';
 import { PrismaCountryRepository } from '../repositories/prisma/prisma-countries-repository';
 import { DeleteAllCountries } from './delete.coutries.database';
 
@@ -16,26 +14,19 @@ export async function GetCoutries() {
     const response = await axios.get(apiUrl);
     
     // Process the data and filter out falsy values
-    const countries = response.data.map((country: any) => {
-      return {
-        name: country.name.common,
-        flag: country.flags[1],
-      };
-    }).filter(Boolean);
+    const countries = response.data.map(country => ({
+      name: country.name.common,
+      flag: country.flags[1],
+    })).filter(Boolean)
+      .sort((a, b) => a.name.localeCompare(b.name));
     
     // Loop through countries and interact with the database
     for (const country of countries) {
-      const existingCountry = await countriesRepository.findByName(country.name);
-      if (existingCountry) {
-        await countriesRepository.updateCountries(existingCountry.countryName, country.flag);
-      } else {
-        await countriesRepository.create({
-          countryName: country.name,
-          CoutryFlag: country.flag,
-        });
-      }
+      await countriesRepository.create({
+        countryName: country.name,
+        CoutryFlag: country.flag,
+      });
     }
-
   } catch (error) {
     console.error('Error fetching or processing countries:', error);
   }
