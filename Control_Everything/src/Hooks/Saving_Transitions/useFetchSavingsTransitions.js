975f6b12fa0b0c842cbae7325e24e4a9a0a82a24
Tiago Sousa <ttiagopintosousa97@gmail.com>
2023-12-28 05:36:45 +0000

    Control_Everything - get all savings transitions from database

---
+++ Control_Everything/src/Hooks/Saving_Transitions/useFetchSavingsTransitions.js
@@ -0,0 +1,38 @@
+import { useState, useEffect, useContext } from 'react';
+import { BASE_URL } from '../../config/urls';
+import axios from 'axios';
+import DataBaseContext, { DataBaseState } from '../../Contexts/DataBase_Context';
+
+const useFetchSavingsTransitions = () => {
+ 
+  const [ savingTransitionsList, setSavingTransitionsList ] = useState(null);
+  const [ perPage, setPerPage] = useState(10);
+  const [ page, setPage ] = useState(1);
+  const { authenticated, userId } = DataBaseState();
+  
+  useEffect(() => {
+    if (!authenticated) return;
+
+    const fetchTotalTransitions = async () => {
+      try {
+        const token = sessionStorage.getItem('token');
+        const response = await axios.get(`
+          ${BASE_URL}/saving-transitions/${userId}/get-savings-transitions?perPage=${perPage}&page=${page}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+        });
+
+        setSavingTransitionsList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTotalTransitions();
+  }, [authenticated, userId, page, perPage]); // Dependências do efeito
+
+  return {savingTransitionsList, setSavingTransitionsList};
+}
+
+export default useFetchSavingsTransitions;
--- Control_Everything/src/Pages/Private/Savings_Page/Savings_History/Savings_History.jsx
+++ Control_Everything/src/Pages/Private/Savings_Page/Savings_History/Savings_History.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Savings_History.scss';
+import useFetchSavingsTransitions from '../../../../Hooks/Saving_Transitions/useFetchSavingsTransitions';
 
 const Savings_History = () => {
+
+  const {savingTransitionsList} = useFetchSavingsTransitions()
+  console.log(savingTransitionsList)
+
   return (
-    <div className='Savings_History'></div>
+    <div className='Savings_History'>
+
+    </div>
   )
 };
 
