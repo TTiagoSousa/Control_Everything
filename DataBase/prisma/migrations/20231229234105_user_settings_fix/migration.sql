cda51138607c0786a580cdf52daa74d18700b11c
Tiago Sousa <ttiagopintosousa97@gmail.com>
2023-12-29 23:48:10 +0000

    DataBase - User Settings Migration

---
+++ DataBase/prisma/migrations/20231229234105_user_settings_fix/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX "UserSettings_userId_key";
+
+-- AddForeignKey
+ALTER TABLE "UserSettings" ADD CONSTRAINT "UserSettings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
