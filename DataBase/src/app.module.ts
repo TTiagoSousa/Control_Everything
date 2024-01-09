ca2c48ec8ea9d2d5791b7eb9d028b023b0d6ea1f
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-01-09 18:23:50 +0000

    DataBase - Crypto service, controller and module

--- DataBase/src/app.module.ts
+++ DataBase/src/app.module.ts
@@ -8,9 +8,10 @@ import { EmailModule } from './email/email.module';
 import { SavingTransitionsModule } from './saving_transitions/saving_transitions.module';
 import { CurrencyModule } from './currency/currency.module';
 import { UserSettingsModule } from './user-settings/user-settings.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CountriesModule, EmailModule, SavingTransitionsModule, CurrencyModule, UserSettingsModule],
+  imports: [AuthModule, UserModule, CountriesModule, EmailModule, SavingTransitionsModule, CurrencyModule, UserSettingsModule, CryptoModule],
   controllers: [AppController],
   providers: [AppService],
 })
