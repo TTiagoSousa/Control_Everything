745e73497034ffe70e4266be68c0207d7f430a6c
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-22 00:53:19 +0000

    Header home, add language selector and theme selector

--- src/Containers/Headers/Header_Home/Header_Home.jsx
+++ src/Containers/Headers/Header_Home/Header_Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header_Home.scss';
 import { NavsState } from '../../../Contexts/Navs_Context';
+import Change_Theme from '../../../Components/Selectors/Change_Theme/Change_Theme';
+import Language_Selector from '../../../Components/Selectors/Language_Selector/Language_Selector';
 
 const Header_Home = () => {
 
@@ -13,6 +15,12 @@ const Header_Home = () => {
       <div className="Left_Side">
       </div>
       <div className="Right_Side">
+        <div className='Button_Field'>
+          <Change_Theme />
+        </div>
+        <div className='Button_Field'>
+          <Language_Selector />
+        </div>
       </div>
     </div>
   )
