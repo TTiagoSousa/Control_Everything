55685961698acf8e4951abf47034d6b46d9b718f
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-22 01:51:23 +0000

    Sidebar home

--- src/Containers/Headers/Header_Home/Header_Home.jsx
+++ src/Containers/Headers/Header_Home/Header_Home.jsx
@@ -3,16 +3,41 @@ import './Header_Home.scss';
 import { NavsState } from '../../../Contexts/Navs_Context';
 import Change_Theme from '../../../Components/Selectors/Change_Theme/Change_Theme';
 import Language_Selector from '../../../Components/Selectors/Language_Selector/Language_Selector';
+import { Sling as Hamburger } from 'hamburger-react';
+import * as Color from '../../../Styles/Colors';
 
 const Header_Home = () => {
 
-  const { typeOfNavifation } = NavsState();
+  const { typeOfNavifation, sidebar_Home, setSidebar_Home } = NavsState();
 
   const headerClass = typeOfNavifation === 'Sidebar_Home' ? 'Header_Home With_SideBar' : 'Header_Home';
 
   return (
     <div className={headerClass}>
       <div className="Left_Side">
+        <div className='Menu'>
+          {
+            typeOfNavifation === 'Sidebar_Home' ? (
+              <div className="Hamburger">
+                <Hamburger 
+                  toggled={sidebar_Home}
+                  toggle={setSidebar_Home}
+                  size={20}
+                  color={Color.blue_darker}
+                />
+              </div>
+            ) :
+            typeOfNavifation === 'Mobile_Menu' ?  (
+              <button
+                
+              >
+                <div></div>
+                <div></div>
+                <div></div>
+              </button>
+            ): null
+          }
+        </div>
       </div>
       <div className="Right_Side">
         <div className='Button_Field'>
--- src/Imports/icons.js
+++ src/Imports/icons.js
@@ -9,3 +9,7 @@ export { default as Sun_N1 } from '../Assets/Icons/Sun/Sun_N1';
 export { default as World } from '../Assets/Icons/World/World';
 export { default as Close_Eye } from '../Assets/Icons/Eye/CLose_Eye';
 export { default as Open_Eye } from '../Assets/Icons/Eye/Open_Eye';
+export { default as Simple_Triangle } from '../Assets/Icons/Triangle/Simple_Triangle';
+export { default as GLobal_SVG } from '../Assets/Icons/GLobal_SVG';
+export { default as Dashboard_Cubes } from '../Assets/Icons/Dashboard/Dashboard_Cubes';
+export { default as Settings } from '../Assets/Icons/Settings/Settings';
