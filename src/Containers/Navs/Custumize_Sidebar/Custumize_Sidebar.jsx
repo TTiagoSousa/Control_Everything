4d36681aecf7c0c6e651cd17c5f3de3517847385
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-22 02:12:34 +0000

    Custumize sidebar, change sidebar color

--- src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
+++ src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
@@ -5,15 +5,20 @@ import { NavsState } from '../../../Contexts/Navs_Context';
 import Header from './Components/Header';
 import { DataBaseState } from '../../../Contexts/DataBase_Context';
 import Type_Of_Navigations from './Components/Type_Of_Navigations';
+import Diviser from './Components/Diviser';
+import Choose_Color from './Components/Choose_Color';
 
 const Custumize_Sidebar = () => {
 
-  const { customize_Sidebar, setCustomize_Sidebar } = NavsState();
+  const { customize_Sidebar, setCustomize_Sidebar, typeOfNavifation } = NavsState();
 
   const { authenticated } = DataBaseState();
 
   const currentLocation = window.location.pathname;
+
   const isHomePage = currentLocation === "/CE" || currentLocation.startsWith("/CE/");
+
+  const Menu_Options_Show = typeOfNavifation === "Mobile_Menu" || typeOfNavifation === "Top_Nav_Navigation"
 
   return (
     <>
@@ -27,7 +32,15 @@ const Custumize_Sidebar = () => {
         <Header />
         <div className='Settings_Container'>
           {authenticated && isHomePage && (
-            <Type_Of_Navigations />
+            <>
+              <Type_Of_Navigations />
+              {!Menu_Options_Show && 
+                <>
+                  <Diviser />
+                  <Choose_Color />
+                </>
+              }
+            </>
           )}
         </div>
       </nav>
