87ed9973276d56e8a1ff55b402c0b956c637fdbb
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-22 02:07:54 +0000

    Custumize sidebar, type of navigation

--- src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
+++ src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
@@ -3,10 +3,17 @@ import './Custumize_Sidebar.scss';
 import Dark_Div from '../../../Components/Dark_Div/Dark_Div';
 import { NavsState } from '../../../Contexts/Navs_Context';
 import Header from './Components/Header';
+import { DataBaseState } from '../../../Contexts/DataBase_Context';
+import Type_Of_Navigations from './Components/Type_Of_Navigations';
 
 const Custumize_Sidebar = () => {
 
   const { customize_Sidebar, setCustomize_Sidebar } = NavsState();
+
+  const { authenticated } = DataBaseState();
+
+  const currentLocation = window.location.pathname;
+  const isHomePage = currentLocation === "/CE" || currentLocation.startsWith("/CE/");
 
   return (
     <>
@@ -19,6 +26,9 @@ const Custumize_Sidebar = () => {
       <nav className={`Custumize_Sidebar ${customize_Sidebar ? 'active' : ''}`}>
         <Header />
         <div className='Settings_Container'>
+          {authenticated && isHomePage && (
+            <Type_Of_Navigations />
+          )}
         </div>
       </nav>
     </>
