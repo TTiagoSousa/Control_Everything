61a6ebc92b1759a675cd2fc1b9081fe4487c62c6
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-24 20:21:53 +0000

    Dashboard, assets card

--- src/Pages/Private/Dashboard/Dashboard.jsx
+++ src/Pages/Private/Dashboard/Dashboard.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import './Dashboard.scss';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const Dashboard = () => {
+
+  const { t } = useTranslation();
 
   return (
 
     <div className="Dashboard">
-      regerth
+      <div className='Assets_Card'>
+        <header>
+          <span>{t("Type of Assets")}</span>
+        </header>
+        <body>
+          <ul>
+            <li>
+              <Link className='Title' to="/CE/Savings_Dashboard">{t("Savings Account")}</Link> 
+              <div>
+                <span>XXXXX</span>
+                <span>X</span>
+              </div>
+            </li>
+            <li>
+              <Link className='Title' to="/CE/Crypto_Dashboard">{t("Crypto")}</Link> 
+              <div>
+                <span>XXXXXX</span>
+                <span>X</span>
+              </div>
+            </li>
+          </ul>
+        </body>
+        <footer>
+          <span>{t("Choose the asset")}</span>
+        </footer>
+      </div>
     </div>
 
   )
