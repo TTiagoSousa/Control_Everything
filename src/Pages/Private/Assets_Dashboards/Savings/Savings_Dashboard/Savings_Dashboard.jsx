29c501af63bfd63480de35127a70b0bf3e2dc6e2
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-27 02:50:12 +0000

    Savings dashboard page

---
+++ src/Pages/Private/Assets_Dashboards/Savings/Savings_Dashboard/Savings_Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Savings_Dashboard.scss';
+
+const Savings_Dashboard = () => {
+  return (
+    <div className='Savings_Dashboard'>
+      
+    </div>
+  )
+}
+
+export default Savings_Dashboard;
