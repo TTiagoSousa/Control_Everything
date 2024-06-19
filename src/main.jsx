bfe09473e447b760e00a366aa79353975641d6b2
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-19 18:57:24 +0100

    I18next config

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import ThemeContext from './Contexts/Theme_Context.jsx';
+import './i18n/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
