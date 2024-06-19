8d527224be3b3035f130c2231ea2c8c539112886
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-19 19:00:57 +0100

    Index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.scss';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import * as Public_Page from './Imports/public.pages';
 
 function App() {
   return (
     <>
-
+      <main>
+        <Routes>
+          <Route index element={<Public_Page.Index />} />
+        </Routes>
+      </main>
     </>
   )
 };
---
+++ src/Imports/images.js
@@ -0,0 +1,8 @@
+export { default as Bitcoin } from '../Assets/Imgs/Assets_Types/Crypto/Bitcoin.png';
+export { default as Ethereum } from '../Assets/Imgs/Assets_Types/Crypto/Ethereum.png';
+export { default as Tether } from '../Assets/Imgs/Assets_Types/Crypto/Tether.png';
+export { default as BNB } from '../Assets/Imgs/Assets_Types/Crypto/BNB.png';
+export { default as Link } from '../Assets/Imgs/Assets_Types/Crypto/Link.png';
+export { default as Solana } from '../Assets/Imgs/Assets_Types/Crypto/Solana.png';
+export { default as Dogecoin } from '../Assets/Imgs/Assets_Types/Crypto/Dogecoin.png';
+export { default as Matic } from '../Assets/Imgs/Assets_Types/Crypto/Matic.png';
