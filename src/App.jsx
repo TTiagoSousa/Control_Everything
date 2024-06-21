396d3a028153a3ea5bb74c180ab122271693b13f
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-21 15:48:02 +0100

    Reset password page, and function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import * as Public_Page from './Imports/public.pages';
 import * as Private_Page from './Imports/private.pages';
 import Authentication_Check from './Authentication/Authentication_Check';
 import { DataBaseState } from './Contexts/DataBase_Context';
+import * as Intermediate_Page from './Imports/intermediary.pages';
 
 function App() {
 
@@ -13,10 +14,14 @@ function App() {
     <>
       <main>
         <Routes>
+          
           <Route index element={<Public_Page.Index />} />
           <Route path='Sign_In' element={ authenticated ? <Navigate to="/" /> :  <Public_Page.Sign_In /> } />
           <Route path='Sign_Up' element={ authenticated ? <Navigate to="/" /> :  <Public_Page.Sign_Up /> } />
           <Route path="Recover_Password" element={authenticated ? <Navigate to="/" /> : <Public_Page.Recover_Password />} />
+
+          <Route path="Reset_Password/:token" element={authenticated ? <Navigate to="/" /> : <Intermediate_Page.Reset_Password />} />
+
           <Route
             path="/CE/*"
             element={
--- src/Imports/intermediary.pages.js
+++ src/Imports/intermediary.pages.js
@@ -1,1 +1,2 @@
 export { default as Loading_Page } from '../Pages/Intermediary/Loading_Page/Loading_Page';
+export { default as Reset_Password } from '../Pages/Intermediary/Reset_Password/Reset_Password';
