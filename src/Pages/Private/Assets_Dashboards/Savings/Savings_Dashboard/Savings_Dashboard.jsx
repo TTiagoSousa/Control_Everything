9c6899d86ebeb5b3085e820281f34aad29cf3a1f
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-26 15:07:20 +0100

    Savings dashboad

--- src/Pages/Private/Assets_Dashboards/Savings/Savings_Dashboard/Savings_Dashboard.jsx
+++ src/Pages/Private/Assets_Dashboards/Savings/Savings_Dashboard/Savings_Dashboard.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import './Savings_Dashboard.scss';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import Global_Button from '../../../../../Components/Buttons/Global_Button/Global_Button';
 
 const Savings_Dashboard = () => {
+
+  const { t } = useTranslation();
+
   return (
-    <div className='Savings_Dashboard'>Savings_Dashboard</div>
+    <div className='Savings_Dashboard'>
+
+      <div className='Totals'>
+        <div className='Total_On_Savings'>
+          <div className="Header">
+            <span>{t("Total on savings")}</span>
+          </div>
+          <div className="Body">
+            <span>0000</span>
+            <span>$</span>
+          </div>
+          <div className="Footer">
+            <Link to="Savings_Transitions_History">{t("View history")}</Link>
+          </div>
+        </div>
+        <div className='Savings_Pie_Chart'></div>
+      </div>
+      <div className='Actions'>
+        <div className="Button_Field">
+          <Global_Button 
+            Text={t("Create Transition")}
+          />
+        </div>
+        <div className="Button_Field">
+          <Global_Button 
+            Text={t("Transfer")}
+          />
+        </div>
+      </div>
+    </div>
   )
 };
 
